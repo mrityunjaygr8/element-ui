51e9c886f861eb603df11bf916b9ced60b8d3344
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-10-30 19:19:47 +0530

    router and vuex added

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HelloWorld from "/src/components/HelloWorld.vue";
 import Second from "/src/components/Second.vue";
+import Posts from "/src/components/Posts.vue";
 
 const routes = [
-		{path: "/", component: HelloWorld},
-		{path: "/second", component: Second},
+		{path: "/", component: HelloWorld, name: "home"},
+		{path: "/second", component: Second, name: "second"},
+		{path: "/posts", component: Posts, name: "posts"},
 ]
 
 const router = createRouter({
